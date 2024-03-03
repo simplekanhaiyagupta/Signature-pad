ab49b09d4ce06a5514c11084e45b22903fea3ba5
KANHAIYA KUMAR <123774172+simplekanhaiyagupta@users.noreply.github.com>
2024-03-03 17:13:53 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    var canvas = document.createElement('canvas');
+    canvas.id = 'canvas';
+    canvas.width = 760;
+    canvas.height = 400;
+    
+    
+    
+
+    var signaturePad = new SignaturePad(canvas);
+    var wrapper = document.getElementById('signature-pad');
+    wrapper.appendChild(canvas);
+
+    var clearButton = document.getElementById('clear-btn');
+    clearButton.addEventListener( 'click', function(){
+        signaturePad.clear();
+    });
+
+    var save = document.getElementById('download-btn');
+    save.addEventListener('click', function(){
+        if (signaturePad.isEmpty()) {
+            alert("Please Provide a Signature.");
+        } else {
+            var dataURL = signaturePad.toDataURL();
+            download(dataURL, 'signature.png');
+        }
+    });
+
+    function download(dataURL, location) {
+        var link = document.createElement('a');
+        link.href = dataURL;
+        link.download = location;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Signature-pad/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
